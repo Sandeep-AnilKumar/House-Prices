c4691969c832b21b5c55ae3ec4aeed858f150350
Sandeep Anil Kumar <sandeepa@yahoo-inc.com>
2017-04-07 18:04:51 -0700

    Used pandas for df and also Gradient Boost Regressor

--- Normalize-Data.py
+++ Normalize-Data.py
@@ -1,36 +1,50 @@
 import csv
+import pandas as pd
 import matplotlib.pyplot as plt
 import math
+import time
+from sklearn.ensemble import GradientBoostingRegressor as gBR
+import time
+from sklearn.utils import shuffle
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
 
+start_time = time.time()
 
-train_csv = csv.reader(open('train.csv','rt'), delimiter=',')
-train_data = list()
+train_df = pd.read_csv('train.csv')
+train_df_dict = dict()
 
-for data in train_csv:
-    train_data.append(data)
+length = len(train_df)
+# print train_df.iloc[:1, :]
 
-length = len(train_data)
-sale_prices = list()
+for i in range(length):
+     train_df_dict[train_df.loc[i, 'Id']] = train_df.loc[i, 'SalePrice']
+     train_df.loc[i, 'SalePrice'] = math.log(float(train_df.loc[i, 'SalePrice']))
 
-for i in range(1, length):
-    sale_prices.append(int(train_data[i][80]))
+# print train_df.iloc[:1, :]
 
-X = sale_prices
-Y = list(i for i in range(0, len(sale_prices)))
+col = list(c for c in train_df.columns)
+col = col[1: len(col) - 1]
 
-plt.hist(X, bins=20)
-plt.xlabel("sale_prices")
-plt.ylabel("frequency")
-plt.title("Raw Sale Price Plot")
-plt.show()
+for i in col:
+     train_df.loc[:, i] = train_df.loc[:, i].astype('category')
 
-log_sale_prices = list()
-for price in sale_prices:
-    log_sale_prices.append(math.log(price))
+categorical_columns = train_df.select_dtypes(['category']).columns
+train_df[categorical_columns] = train_df[categorical_columns].apply(lambda x: x.cat.codes)
 
-plt.hist(log_sale_prices, bins=20)
-plt.xlabel("log_sale_prices")
-plt.ylabel("frequency")
-plt.title("Log Sale Price Plot")
-plt.show()
+# print train_df.iloc[:1, :]
+
+train_df = train_df.sample(frac=1).reset_index(drop=True)
+# print train_df.iloc[:1, :]
+
+train_df_vector = train_df.iloc[:, 1: len(col)]
+train_df_target = train_df.iloc[:, len(col) + 1]
+
+reg = gBR(loss='ls', n_estimators=1000)
+reg = reg.fit(train_df_vector, train_df_target)
+
+lr_predict = reg.predict(train_df_vector)
+lr_accuracy = metrics.mean_absolute_error(train_df_target, lr_predict)
+print("Time for the regressor to train and predict on the training data subset is := %.2f" % (time.time() - start_time))
+print("Accuracy is := " + str(lr_accuracy))
 
