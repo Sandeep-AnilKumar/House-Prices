1c73cb85e5ca278398eb00ff22fb2127d3102db4
Sandeep Anil Kumar <sandeepa@yahoo-inc.com>
2017-04-10 10:40:48 -0700

    Completed the test data prediction.

--- Normalize-Data.py
+++ Normalize-Data.py
@@ -1,13 +1,9 @@
-import csv
 import pandas as pd
-import matplotlib.pyplot as plt
 import math
-import time
 from sklearn.ensemble import GradientBoostingRegressor as gBR
 import time
-from sklearn.utils import shuffle
 from sklearn import metrics
-from sklearn.model_selection import train_test_split
+import csv
 
 start_time = time.time()
 
@@ -15,13 +11,10 @@ train_df = pd.read_csv('train.csv')
 train_df_dict = dict()
 
 length = len(train_df)
-# print train_df.iloc[:1, :]
 
 for i in range(length):
      train_df_dict[train_df.loc[i, 'Id']] = train_df.loc[i, 'SalePrice']
-     train_df.loc[i, 'SalePrice'] = math.log(float(train_df.loc[i, 'SalePrice']))
-
-# print train_df.iloc[:1, :]
+     train_df.loc[i, 'SalePrice'] = math.log10(float(train_df.loc[i, 'SalePrice']))
 
 col = list(c for c in train_df.columns)
 col = col[1: len(col) - 1]
@@ -32,19 +25,50 @@ for i in col:
 categorical_columns = train_df.select_dtypes(['category']).columns
 train_df[categorical_columns] = train_df[categorical_columns].apply(lambda x: x.cat.codes)
 
-# print train_df.iloc[:1, :]
 
 train_df = train_df.sample(frac=1).reset_index(drop=True)
-# print train_df.iloc[:1, :]
 
 train_df_vector = train_df.iloc[:, 1: len(col)]
 train_df_target = train_df.iloc[:, len(col) + 1]
 
-reg = gBR(loss='ls', n_estimators=1000)
+reg = gBR(loss='ls', n_estimators=10000)
 reg = reg.fit(train_df_vector, train_df_target)
 
 lr_predict = reg.predict(train_df_vector)
 lr_accuracy = metrics.mean_absolute_error(train_df_target, lr_predict)
 print("Time for the regressor to train and predict on the training data subset is := %.2f" % (time.time() - start_time))
 print("Accuracy is := " + str(lr_accuracy))
+
+start_time = time.time()
+test_df = pd.read_csv('test.csv')
+test_data_ids = list(test_df['Id'])
+
+length = len(test_df)
+
+col = list(c for c in test_df.columns)
+col = col[1:]
+
+for i in col:
+     test_df.loc[:, i] = test_df.loc[:, i].astype('category')
+
+categorical_columns = test_df.select_dtypes(['category']).columns
+test_df[categorical_columns] = test_df[categorical_columns].apply(lambda x: x.cat.codes)
+
+test_df_vector = test_df.iloc[:, 1: len(col)]
+
+reg = gBR(loss='ls', n_estimators=10000)
+reg = reg.fit(train_df_vector, train_df_target)
+
+lr_predict = reg.predict(test_df_vector)
+print("Time for the regressor to train and predict on the testing data subset is := %.2f" % (time.time() - start_time))
+
+csv_file = open("submissions.csv", 'w')
+wr = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_NONE)
+wr.writerow(['Id', 'SalePrice'])
+
+for index in range(0, len(test_data_ids)):
+    wr.writerow([test_data_ids[index], math.pow(10, lr_predict[index])])
+    index += 1
+print("Done with predicting Sale Price values for the test data")
+csv_file.close()
 
